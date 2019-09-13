4065da3ddb410144771b176af93cb33615640b37
Marisha Gnanaseelan <magn@nmbu.no>
2019-09-13 09:45:02 +0200

    From comprehension to loop

--- src/marisha_gnanaseelan_ex/ex01/comp_to_loop.py
+++ src/marisha_gnanaseelan_ex/ex01/comp_to_loop.py
@@ -1,14 +1,18 @@
 
-function squares_by_loop()
-
 def squares_by_comp(n):
     return [k**2 for k in range(n) if k % 3 == 1]
 
 
-
-
+def squares_by_loop(n):
+    loop_list = []
+    for k in range(n):
+        if k % 3 == 1:
+            loop_list.append(k ** 2)
+    return loop_list
 
 
 if __name__ == '__main__':
-    if deck_loop() != deck_comp():
+    if squares_by_comp(10) != squares_by_loop(10):
         print('ERROR!')
+    else:
+        print("squares_by_comp() and squares_by_loop() are the same!")
