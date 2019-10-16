6519d0edc15315a58665b284c966be64d65ed1bc
Marisha Gnanaseelan <magn@nmbu.no>
2019-10-16 19:36:32 +0200

    Added snakes_and_ladders.py file

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -4,58 +4,65 @@ __author__ = 'Marisha Gnanaseelan', 'Peter Langdalen'
 __email__ = 'magn@nmbu.no', 'pelangda@nmbu.no'
 
 
-class SnakesAndLadders:
+def single_game(num_players):
+    """
+    Returns duration of single game.
 
-    def __init__(self):
-        pass
+    Arguments
+    ---------
+    num_players : int
+        Number of players in the game
 
-    def single_game(num_players):
-        """
-        Returns duration of single game.
+    Returns
+    -------
+    num_moves : int
+        Number of moves the winning player needed to reach the goal
+    """
+    num_players = input(int('How many players? '))
+    position = 0
+    num_moves = 0
 
-        Arguments
-        ---------
-        num_players : int
-            Number of players in the game
+    if num_players < 1:
+        print('Has to be one or more.')
 
-        Returns
-        -------
-        num_moves : int
-            Number of moves the winning player needed to reach the goal
-        """
 
-    def multiple_games(num_games, num_players):
-        """
-        Returns durations of a number of games.
+def multiple_games(num_games, num_players):
+    """
+    Returns durations of a number of games.
 
-        Arguments
-        ---------
-        num_games : int
-            Number of games to play
-        num_players : int
-            Number of players in the game
+    Arguments
+    ---------
+    num_games : int
+        Number of games to play
+    num_players : int
+        Number of players in the game
 
-        Returns
-        -------
-        num_moves : list
-            List with the numbedr of moves needed in each game.
-        """
+    Returns
+    -------
+    num_moves : list
+        List with the numbedr of moves needed in each game.
+    """
 
-    def multi_game_experiment(num_games, num_players, seed):
-        """
-        Returns durations of a number of games when playing with given seed.
 
-        Arguments
-        ---------
-        num_games : int
-            Number of games to play
-        num_players : int
-            Number of players in the game
-        seed : int
-            Seed used to initialise the random number generator
+def multi_game_experiment(num_games, num_players, seed):
+    """
+    Returns durations of a number of games when playing with given seed.
 
-        Returns
-        -------
-        num_moves : list
-            List with the numbedr of moves needed in each game.
-        """
+    Arguments
+    ---------
+    num_games : int
+        Number of games to play
+    num_players : int
+        Number of players in the game
+    seed : int
+        Seed used to initialise the random number generator
+
+    Returns
+    -------
+    num_moves : list
+        List with the numbedr of moves needed in each game.
+    """
+
+
+if __name__ == '__main__':
+    multi_game_experiment()
