c97d5f29468a1654bbb45d285f72c98f6a203eb9
Peter Langdalen <pelangda@nmbu.no>
2019-10-18 08:40:26 +0200

    PA01: added the second function.

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -68,6 +68,11 @@ def multiple_games(num_games, num_players):
     num_moves : list
         List with the numbedr of moves needed in each game.
     """
+    num_moves = [0] * num_games
+
+    for games in range(num_games):
+        num_moves[games] = single_game(num_players)
+    return num_moves
 
 
 def multi_game_experiment(num_games, num_players, seed):
