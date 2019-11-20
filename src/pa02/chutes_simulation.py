a69670e913f3b27ebb1429d370d5dc287c237f3b
Marisha Gnanaseelan <magn@nmbu.no>
2019-11-20 10:06:15 +0100

    PA02: Started on "class Simulation"

--- src/pa02/chutes_simulation.py
+++ src/pa02/chutes_simulation.py
@@ -66,7 +66,7 @@ class Board:
         chute or ladder, the method returns 0.
 
         :param: position
-        :return: self.new_position or 0 
+        :return: self.new_position or 0
         """
         if position in self.ladders.keys():
             self.new_position = self.ladders[position] - position
@@ -148,3 +148,54 @@ class LazyPlayer(Player):
             if self.player_position <= 0:
                 self.player_position = start_position
         return self.player_position
+
+class Simulation:
+    def __init__(self, player_field, board, seed=123, randomize_players=False):
+        self.player_field = player_field
+        self.board = Board()
+        self.randomize_players = randomize_players
+        self.result = []
+        random.seed(seed)
+
+    def single_game(self):
+        """
+        Runs a single game returning a tuple consisting of the number of moves
+        made and the type of the winner.
+        :return:
+        """
+        position = [0] * self.list_of_players
+        num_moves = 0
+
+        while max(position) < self.Board.goal:
+            for player in range(len(position)):
+                position[player] += self.Player.roll_dice
+
+                if position[player] in snakes_and_ladders:
+                    position[player] = snakes_and_ladders[position[player]]
+            num_moves += 1
+
+        return (num_moves, self.type_of_player)
+
+    def run_simulation(self):
+
+        for game in range(num_games):
+            self.result.append(self.single_game())
+
+    def get_results(self):
+        return self.result()
+
+    def winners_per_type(self):
+        for _ in self.type_of_player:
+            winners_per_type = {self.type_of_player: num_of_wins}
+        return winners_per_type
+
+    def durations_per_type(self):
+        pass
+
+    def players_per_type(self):
+        pass
+
+
+
+
+
